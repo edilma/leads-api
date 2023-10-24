b08b3107590a9cb567ee4a8ee141ccb1ebcfa528
Edilma Riano <edilmaria@hotmail.com>
2023-10-24 15:55:29 -0400

    DEPLOYED to firebase - using MongoDB

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,33 @@
 import functions from "firebase-functions";
 import express from "express";
-import cors from "cors"
-import {getAllLeads, addLead, getLeadsByLocation} from "./src/leads.js"
-import {getAllBuyers, addBuyer, getBuyersByRequest, loginBuyer} from "./src/buyers.js"
+import cors from "cors";
+import { getAllLeads, addLead, getLeadsByLocation } from "./src/leads.js";
+import {
+  getAllBuyers,
+  addBuyer,
+  getBuyersByRequest,
+  loginBuyer,
+} from "./src/buyers.js";
 
 const app = express();
-app.use (cors());
+app.use(cors());
 
-app.get("/test", (req,res)=>{
-    res.send("The leads api es connected");
+app.get("/test", (req, res) => {
+  res.send("The leads api es connected");
 });
 //API points for leads
-app.get("/leads", await getAllLeads)
-app.post ("/leads", addLead)
-app.get("/leads/:type", getLeadsByLocation)
+app.get("/leads", await getAllLeads);
+app.post("/leads", addLead);
+app.get("/leads/:type", getLeadsByLocation);
 
 //API points for buyers
-app.get("/buyers", await getAllBuyers)
-app.post ("/buyers", addBuyer)
-app.post("/login" , loginBuyer)
+app.get("/buyers", await getAllBuyers);
+app.post("/buyers", addBuyer);
+app.post("/login", loginBuyer);
 
 //the get buyers by location needs to be modify
-app.get("/buyers/:type", getBuyersByRequest)
+app.get("/buyers/:type", getBuyersByRequest);
 
 //the API is hosted in https://lds-leads-er.web.app
 
-export const api= functions.https.onRequest(app)
+export const api = functions.https.onRequest(app);
